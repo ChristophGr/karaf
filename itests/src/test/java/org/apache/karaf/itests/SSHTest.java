815237fce97aca45be4758d500679adc428744dd
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2012-12-22 16:50:22 +0100

    move SSHTest to itests

--- itests/src/test/java/org/apache/karaf/itests/SSHTest.java
+++ itests/src/test/java/org/apache/karaf/itests/SSHTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.karaf.tooling.exam.regression;
+package org.apache.karaf.itests;
 
 import org.apache.karaf.tooling.exam.container.internal.runner.KarafTestRunner;
 import org.apache.karaf.tooling.exam.options.ConfigurationPointer;
